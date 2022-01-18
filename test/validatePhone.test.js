3bddc46be1abcc43a42d65ac4f017c081c3d4759
iamprincesly <iamprincesly@gmail.com>
2022-01-18 01:45:40 +0100

    update test

--- test/validatePhone.test.js
+++ test/validatePhone.test.js
@@ -18,31 +18,21 @@ var SMS = new BulkSMSNigeria(
     2
 );
 
-describe('Validat Phone', () => {
+describe('Validate Phone Number', () => {
     it('Should throw error for invalid phone number', () => {
         expect(() => {
-            SMS.validatePhone('701234567867,07065561062341,234706106');
+            SMS.checkNumbers('701234567867,07065561062341,234706106');
         }).toThrow();
     });
 
     it('Should throw error for containing letters and special characters', () => {
         expect(() => {
-            SMS.validatePhone('7065526f06,07012]45678,23470655-106');
-        }).toThrow();
-    });
-
-    it('Should throw error because numbers are not pass as string', () => {
-        expect(() => {
-            SMS.validatePhone(['7012345678','07012345678','2347012345678']);
-        }).toThrow();
-
-        expect(() => {
-            SMS.validatePhone(2347012345678);
+            SMS.checkNumbers('7065526f06,07012]45678,23470655-106');
         }).toThrow();
     });
 
     it('Should return valid phone numbers seperated with a comma', () => {
-        var result = SMS.validatePhone('7012345678,07012345678,2347012345678');
+        var result = SMS.checkNumbers('7012345678,07012345678,2347012345678');
         expect(result).toBe('7012345678,07012345678,2347012345678');
     });
 });
